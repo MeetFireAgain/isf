5735b70fe1f9b361303a62f8a2439424404e5c66
MeetFireAgain <55641631+MeetFireAgain@users.noreply.github.com>
2022-10-17 18:10:52 +0800

    add more...

--- isf.py
+++ isf.py
@@ -5,12 +5,12 @@ from __future__ import print_function
 import argparse
 import logging.handlers
 import os
-import ConfigParser
+import configparser
 from icssploit.interpreter import IcssploitInterpreter
 
 # Define conf
 isf_conf_file = "isf.ini"
-isf_conf = ConfigParser.SafeConfigParser(allow_no_value=True)
+isf_conf = configparser.SafeConfigParser(allow_no_value=True)
 isf_conf.read(isf_conf_file)
 
 # Get parameter from conf
